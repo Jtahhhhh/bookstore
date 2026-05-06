463d7692d85c0637ad676127092462061bfeb048
Jtahhhhh <98197418+Jtahhhhh@users.noreply.github.com>
2026-05-06 10:06:40 +0700

    init test

--- test/models/flash_sale_item_test.rb
+++ test/models/flash_sale_item_test.rb
@@ -1,15 +1,19 @@
 require "test_helper"
 
 class FlashSaleItemTest < ActiveSupport::TestCase
-  test "quantity must be greater than or equal to 0" do
-    item = ReservationItem.new(
-      quantity: 0,
-      unit_price: 100,
-      subtotal: 0
-    )
+  test "stock must not be negative" do
+    item = flash_sale_items(:ruby_sale_item)
+    item.stock = -1
 
     assert_not item.valid?
-    
-    assert_includes item.errors[:quantity], "must be greater than 0"
+    assert_includes item.errors[:stock], "must be greater than or equal to 0"
+  end
+
+  test "sale price must not be negative" do
+    item = flash_sale_items(:ruby_sale_item)
+    item.sale_price = -1
+
+    assert_not item.valid?
+    assert_includes item.errors[:sale_price], "must be greater than or equal to 0"
   end
 end
--- test/models/reservation_item_test.rb
+++ test/models/reservation_item_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class ReservationItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "quantity must be greater than zero" do
+    item = reservation_items(:pending_reservation_item)
+    item.quantity = 0
+
+    assert_not item.valid?
+    assert_includes item.errors[:quantity], "must be greater than 0"
+  end
+
+  test "subtotal must not be negative" do
+    item = reservation_items(:pending_reservation_item)
+    item.subtotal = -1
+
+    assert_not item.valid?
+    assert_includes item.errors[:subtotal], "must be greater than or equal to 0"
+  end
 end
